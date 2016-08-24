d134e6cafb482fd3f6f20be7df37a7a9275ae5a7
Schyler Hosler <schylerhosler@gmail.com>
2016-08-23 23:08:35 -0400

    Initial commit:

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,28 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @ingredients = Ingredient.all
+    @recipe.ingredients.build#(name: ???) #no argument?
+    @recipe.ingredients.build#(name: ???)
   end
 
   def create
+    Recipe.create(recipe_params)
+    redirect_to recipes_path
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+    @ingredients = Ingredient.all
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    redirect_to recipes_path
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:id, :name, :quantity])
   end
 end
